1f204cb83a7415dc9dfe8c56d02a02ba603cf01d
Hoa Pham <hoaqb208@gmail.com>
2025-05-05 14:18:40 +0700

    TextBox & ComboBox

---
+++ Libs.WPF/Test.WPF/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Libs.WPF.MVVM;
+
+namespace Test.WPF
+{
+    class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            _SelectedStringItem = StringList[0];
+        }
+
+        public List<string> StringList { get; } = new List<string>()
+        {
+            "Sample 1",
+            "Sample 2",
+            "Sample 3",
+            "Sample 4",
+            "Sample 5",
+            "Sample 6",
+            "Sample 7",
+        };
+
+        private string _SelectedStringItem;
+        public string SelectedStringItem
+        {
+            get { return _SelectedStringItem; }
+            set
+            {
+                _SelectedStringItem = value;
+                OnPropertyChanged(nameof(SelectedStringItem));
+            }
+        }
+
+    }
+}
--- Libs.WPF/Test.WPF/MainWindow.xaml.cs
+++ Libs.WPF/Test.WPF/MainWindow.xaml.cs
@@ -10,6 +10,8 @@ namespace Test.WPF
             IsShowSnapshotButton = true;
             InitializeComponent();
 
+            this.DataContext = new MainViewModel();
+
             this.Height = SystemParameters.PrimaryScreenHeight * 0.7;
             this.Width = this.Height * 1.618;
         }

37ef5a0bdd0219ec3423fd90084b6b75101a2fff
Hoa Pham <hoaqb208@gmail.com>
2025-05-15 14:43:47 +0700

    DatePicker

---
+++ Libs.WPF/Libs.WPF/Converters/StringNullOrEmptyToVisible.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+
+namespace Libs.WPF.Converters
+{
+    public class StringNullOrEmptyToVisible : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.IsNullOrWhiteSpace(value as string) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Visibility.Collapsed;
+        }
+    }
+}

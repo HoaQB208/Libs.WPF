041b180c7b7780c70fbb801cf52c5e12ca0cd8fe
Thái Hoà Phạm <hoaqb208@gmail.com>
2025-06-04 09:28:22 +0700

    DataGridSelectedItemsBehavior

---
+++ Libs.WPF/Libs.WPF/Behavior/DataGridSelectedItemsBehavior.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Libs.WPF.Behavior
+{
+    public class DataGridSelectedItemsBehavior
+    {
+        public static readonly DependencyProperty SelectedItemsProperty =
+            DependencyProperty.RegisterAttached("SelectedItems",
+                typeof(IList),
+                typeof(DataGridSelectedItemsBehavior),
+                new PropertyMetadata(null, OnSelectedItemsChanged));
+
+        public static void SetSelectedItems(DependencyObject element, IList value)
+        {
+            element.SetValue(SelectedItemsProperty, value);
+        }
+
+        public static IList GetSelectedItems(DependencyObject element)
+        {
+            return (IList)element.GetValue(SelectedItemsProperty);
+        }
+
+        private static void OnSelectedItemsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is DataGrid dataGrid)
+            {
+                dataGrid.SelectionChanged -= DataGrid_SelectionChanged;
+                dataGrid.SelectionChanged += DataGrid_SelectionChanged;
+            }
+        }
+
+        private static void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is DataGrid dataGrid)
+            {
+                var boundList = GetSelectedItems(dataGrid);
+                if (boundList != null)
+                {
+                    boundList.Clear();
+                    foreach (var item in dataGrid.SelectedItems)
+                    {
+                        boundList.Add(item);
+                    }
+                }
+            }
+        }
+    }
+}

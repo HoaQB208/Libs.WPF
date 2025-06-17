9079d9605a12614e112f293611f7a013a5d4b1fa
Hoa Pham <hoaqb208@gmail.com>
2025-06-17 13:44:52 +0700

    Update Styles

---
+++ Libs.WPF/Libs.WPF/Converters/BoolInversion.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System;
+
+namespace Libs.WPF.Converters
+{
+    public class BoolInversion : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool vl) return !vl;
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return false;
+        }
+    }
+}

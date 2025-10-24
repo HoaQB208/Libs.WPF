5bd42e8ff11bb251b01d2480e142211c0de5e807
Hoa Pham <hoaqb208@gmail.com>
2025-10-24 14:20:10 +0700

    Add TrueToCollapsed value converter
    Introduces the TrueToCollapsed converter to map boolean true to Visibility.Collapsed and false to Visibility.Visible. Registers the converter in ConverterStyles.xaml for use in WPF bindings.

---
+++ Libs.WPF/Libs.WPF/Converters/TrueToCollapsed.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+using System;
+
+namespace Libs.WPF.Converters
+{
+    public class TrueToCollapsed : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is true ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Visibility.Collapsed;
+        }
+    }
+}
